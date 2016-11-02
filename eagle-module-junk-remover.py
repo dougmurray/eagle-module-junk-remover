68b9b6f32ee62f5e546853f8beb3aa378f3e56a9
Doug Murray <douglass.murray@gmail.com>
2016-11-01 20:15:39 -0400

    Started python script, template with description.

---
+++ eagle-module-junk-remover.py
@@ -0,0 +1,10 @@
+# eagle-module-junk-remover.py
+#!usr/bin/env python27
+
+"""Removed unused signals in board XML file that CADSoft Eagle produces
+   when modules are imported.
+
+   Author:  Douglass Murray
+   Date:  2016-11-01
+"""
+

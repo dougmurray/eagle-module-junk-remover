315932fe4ed40569f226544de4138066550112db
Doug Murray <murray@twinleaf.com>
2018-11-16 14:18:05 -0500

    Deleting empty data signal tags, the front leaders first.

--- eagleJunkRemover.py
+++ eagleJunkRemover.py
@@ -23,39 +23,8 @@ for i, element in enumerate(editMe):
         if re.match(unnecessaryTail, tester):
             locations.append(i)
 
-"""
-for i, element in enumerate(newestList):
-    for n, data in enumerate(newestList):
-        if n == +i:
-            print "DELETE"
-        else:
-            print "NOTHING"
-"""
-"""
-newList = [m.group(0) for line in read_data for m in [regex.search(line)] if m]
-print newList
-"""
-"""
-for i, data in enumerate(read_data):
-        if data=='<signal1>\n':
-            print i, 'YES!'
-        else:
-            print i, 'NO!'
-"""
+for i in sorted(locations, reverse=True):
+    del editMe[i]
 
-"""Example Operation"""
-someList = [10,11,13,15,17,18,20]
-n = someList[0]
-print n
 
-for i, element in enumerate(someList):
-    while element == n:
-        if i+1 < len(someList) and someList[i+1] == n+1:
-            print "TRUE"
-            n = n+1
-        else:
-            n = n+1
-            print "FALSE"
-    else:
-        n = n+1
 
